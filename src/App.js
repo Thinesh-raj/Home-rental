184748f7336e963aff74be30d4c89a714df29951
Thinesh-raj <thineshraj02@gmail.com>
2024-03-18 23:06:40 +0530

    Home-Rental

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-// import LinkTask1 from './Components/React/Home_Rental/linkAPP';
-// import Property from './Components/React/Home_Rental/Property/Property';
 import Newtask from './Components/React/Newtask/linkapp';
 
 
 function App() {
   return <Newtask />
-  // return <BrowserRouter>
-  //   <Routes>
-  //     <Route path='/' element={<LinkTask1 />} />
-  //     <Route path='/property/:type' element={<Property />} />
-  //   </Routes>
-  // </BrowserRouter>
 
 
 }
